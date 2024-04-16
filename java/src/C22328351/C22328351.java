0068e3cc8964ebbd8ff5c75b4818ee344e66d0e5
Larina Yu <larinaxy2964@gmail.com>
2024-04-16 16:26:49 +0100

    Star Visual

--- java/src/C22328351/C22328351.java
+++ java/src/C22328351/C22328351.java
@@ -1,63 +1,61 @@
 package C22328351;
 
-import java.util.*;
-import java.util.Arrays;
-
 import ddf.minim.AudioBuffer;
-import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
 import processing.core.PApplet;
 
 public class C22328351 extends PApplet{
 
-    public void settings() {
-        fullScreen(P3D, SPAN);
-        println("CWD: " + System.getProperty("user.dir"));
+    float rotX, rotY;
+    float radius = 200;
+    int points = 5;
+
+    public void settings()
+    {
+        size(800, 800, P3D);
     }
 
+    public void setup()
+    {
+        background(0);
+        lights();
 
-    public void setup() {
-        size(800, 200);
-        background(255);
-        drawPiano(10, 10, 25, 150);
+        translate(width/2, height/2);
+
+        rotateX(rotX);
+        rotateY(rotY);
+
+        drawStar(radius, points);
+
+        rotX += 0.01;
+        rotY += 0.02;
     }
-     
-    public void drawPiano(float x, float y, float keyWidth, float keyHeight) {
-        float whiteKeyGap = keyWidth * 0.2f;
-        float blackKeyGap = keyWidth * 0.5f;
-        int[] whiteKeys = {0, 2, 4, 5, 7, 9, 11};
 
+    public void drawStar(float radius, int points)
+    {
+        float angle = TWO_PI / points;
+        float starAngle = angle / 2.0f;
 
-        for(int i = 0; i < 14; i++){
-            float xPos = x + (i * (keyWidth + whiteKeyGap));
-            if(i % 7 !=2 && i % 7 != 6){
-                fill(255);
-                rect(xPos, y, keyWidth, keyHeight);
-            }
-            else{
-                fill(0);
-                int blackKeyWidth = 10; 
-                float blackKeyHeight = 100; 
-                rect(xPos - (blackKeyWidth / 2), y, blackKeyWidth, blackKeyHeight);
-            }
-
-
-            if(Arrays.binarySearch(whiteKeys, i % 7) >= 0){
-                fill(0);
-                text(i/7, (float) (xPos - (keyWidth * 0.5)), y + keyHeight + 12);
-            }
-            else{
-                text(i/7, (float) (xPos + (keyWidth * 0.5)), y + keyHeight + 12);
-            }
+        beginShape();
+        for(float a = 0; a < TWO_PI; a += angle)
+        {
+            float x = cos(a) * radius;
+            float y = sin(a) * radius;
+            float z1 = radius/2;
+            float z2 = -radius/2;
+            vertex(x, y, z1);
+            vertex(0, 0, z2);
+            x = cos(a + starAngle) * radius / 2;
+            y = sin(a + starAngle) * radius / 2;
+            vertex(x, y, z1);
         }
+        endShape(CLOSE);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         PApplet.main("C22328351.C22328351");
     }
+
 }
-
-
-
-
